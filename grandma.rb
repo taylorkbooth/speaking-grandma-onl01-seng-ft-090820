766f4cb727c1407a6f3380789f30bad612746240
Taylor Booth <taylorkbooth@gmail.com>
2020-09-10 13:08:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   elsif phrase != phrase.upcase
-    HUH?! SPEAK UP, SONNY!
-    
+    'HUH?! SPEAK UP, SONNY!'
+  elsif phrase
     
   end
 end
