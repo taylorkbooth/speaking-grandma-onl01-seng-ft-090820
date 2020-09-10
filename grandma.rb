cbbd318dfc51e1579fa206f03c2136de4c0980ee
Taylor Booth <taylorkbooth@gmail.com>
2020-09-10 13:08:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     'I LOVE YOU TOO PUMPKIN!'
   elsif phrase != phrase.upcase
     'HUH?! SPEAK UP, SONNY!'
-  elsif phrase == phrase.upcase
+  elsif phrase == phrase.upcase && phrase
     
   end
 end
