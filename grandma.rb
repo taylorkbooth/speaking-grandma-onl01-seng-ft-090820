4f84c0064dd7d1cc5e0a57972383c6ed26c16ef2
Taylor Booth <taylorkbooth@gmail.com>
2020-09-10 13:09:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(phrase)
   elsif phrase != phrase.upcase
     'HUH?! SPEAK UP, SONNY!'
   elsif phrase == phrase.upcase && phrase != 'I LOVE YOU GRANDMA!'
-    
+    "NO, NOT SINCE 1938!"
   end
 end
